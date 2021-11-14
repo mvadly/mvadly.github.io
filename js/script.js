3a23b489ed96fb935b2ee2ec7638d2bfa914babd
mvadly <mfadly24@gmail.com>
2021-11-14 19:34:38 +0700

    add page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,15 @@
 function getPage(page){
-    console.log(page);
-    if(page === '404'){
-        $('.menu-link a.active').removeClass('active');
-    }else{
-        let el = $('.menu-link a[data-page="'+page+'"]').addClass('active');
-        console.log(el);
-    }
-
+    
     $.ajax({
         type: "get",
         url: "page/"+page+'.html',
         dataType: "html",
         success: function (response) {
+            if(page === '404'){
+                $('.menu-link a.active').removeClass('active');
+            }else{
+                $('.menu-link a[data-page="'+page+'"]').addClass('active');
+            }
             
             $('.content').html(response);
         }
@@ -24,6 +22,7 @@ $(document).ready(function () {
     getPage(active_page);
 
     $('.menu-link a').on('click', function(){
+        $('.menu-link a').removeClass('active');
         let page = $(this).data('page');
         if(page === undefined) page = "404";
         getPage(page);
