6b9f570769d27224be29fa7f37a2abc6b2bc5d7a
mvadly <mfadly24@gmail.com>
2021-11-14 18:02:41 +0700

    add active page

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
 function getPage(page){
+    console.log(page);
+    if(page === '404'){
+        $('.menu-link a.active').removeClass('active');
+    }else{
+        let el = $('.menu-link a[data-page="'+page+'"]').addClass('active');
+        console.log(el);
+    }
+
     $.ajax({
         type: "get",
-        url: "page/"+page,
+        url: "page/"+page+'.html',
         dataType: "html",
         success: function (response) {
-            // if(response == '' || response != undefined){
-            //     response = 'Page not found';
-            // }
-
+            
             $('.content').html(response);
         }
     });
 }
 
 $(document).ready(function () {
+
+    let active_page = $('.menu-link a.active').data('page');
+    getPage(active_page);
+
     $('.menu-link a').on('click', function(){
         let page = $(this).data('page');
-        if(page === undefined) page = "404.html";
+        if(page === undefined) page = "404";
         getPage(page);
     })    
 });
