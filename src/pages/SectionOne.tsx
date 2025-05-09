bd4e2fc885598c50d9301f7d77de34cf4b82af46
Muhammad Fadly <mfadly24@gmail.com>
2025-05-09 16:15:02 +0700

    feat: add tailwindcss

---
+++ src/pages/SectionOne.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SectionOne = () => {
+  return (
+    <div className="w-full h-screen bg-gray-100 flex">
+      <div className="bg-blue-500 w-full">
+        <h1 className="text-3xl font-bold text-white">Hello Vite + React!</h1>
+      </div>
+      <div className="bg-orange-500 w-full">
+        <h1 className="text-3xl font-bold text-white">Hello Vite + React!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default SectionOne;
