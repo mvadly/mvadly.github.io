e654e8f07ae8f5480f79b2a329891cb1a4fe540c
Muhammad Fadly <mfadly24@gmail.com>
2025-05-10 09:27:23 +0700

    fix: mobile view for profile

--- src/components/molecule/MVadly.tsx
+++ src/components/molecule/MVadly.tsx
@@ -1,10 +1,10 @@
 const MVadly = () => {
   return (
     <div className="relative">
-      <div className="text-[3.4em] text-slate-300 font-thin">
+      <div className="text-[2.6em] sm:text-[3.4em] text-slate-300 font-thin">
         MUHAMMAD
       </div>
-      <div className=" text-[6.6em] font-bold text-slate-300 absolute top-5 left-[-4px]">
+      <div className="text-[5em] sm:text-[6.6em] font-bold text-slate-300 absolute top-5 sm:top-5 left-[-3px] sm:left-[-4px]">
         FADLY
       </div>
     </div>
--- src/pages/SectionOne.tsx
+++ src/pages/SectionOne.tsx
@@ -6,32 +6,34 @@ import ImageMV from "../assets/mvdcartoon.png";
 const SectionOne = () => {
   return (
     <div className="w-full sm:h-screen bg-gray-100 sm:flex">
-      <div className="bg-blue-500 w-full px-5 py-5 sm:py-0 sm:px-2">
+      <div className="bg-blue-500 w-full">
         <div className="flex justify-center items-center sm:h-screen">
           <div className="relative">
-            <div className="flex flex-col justify-center w-[500px]">
+            <div className="flex flex-col justify-center sm:w-[500px] px-8 py-5 sm:py-0 sm:px-2">
               <Bubble>it's me</Bubble>
               <MVadly />
 
-              <div className="relative mt-20">
+              <div className="relative mt-16 sm:mt-20">
                 <div className="w-full bg-slate-300 h-1"></div>
                 <div className="text-md sm:text-2xl font-semibold text-slate-300 z-[1] text-center">
                   Software Developer | Backend Developer
                 </div>
                 <div className="w-full bg-slate-300 h-1 mt-1"></div>
               </div>
-              <p className="text-slate-300 font-thin sm:text-xl wrap-break-word sm:mt-5 z-[1]">
+              <p className="text-slate-300 font-thin sm:text-xl wrap-break-word mt-5 sm:mt-5 z-[1]">
                 I am a software developer with a passion for creating innovative
                 solutions. I specialize in backend development and have
                 experience with various technologies and frameworks.
               </p>
             </div>
-            <div className="absolute top-[-78px] right-[-19px] w-[200px]">
+            <div className="absolute top-[-20px] right-[20px] sm:top-[-78px] sm:right-[-10px] w-[150px] sm:w-[200px]">
               <img src={ImageMV} alt="mvadly" className="" />
             </div>
-            <Button onclick={() => alert("its button")} color="orange">
-              <strong>let's talk</strong>
-            </Button>
+            <div className="px-8 pb-2 sm:py-0 sm:px-2">
+              <Button onclick={() => alert("its button")} color="orange">
+                <strong>let's talk</strong>
+              </Button>
+            </div>
           </div>
         </div>
       </div>
