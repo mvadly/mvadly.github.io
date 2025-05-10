7bb864b5c90a30ac0eb7c129fdd7f4286069cac8
Muhammad Fadly <mfadly24@gmail.com>
2025-05-10 11:53:59 +0700

    feat: create mobile responsive for navbar

--- src/components/atom/Bubble.tsx
+++ src/components/atom/Bubble.tsx
@@ -1,12 +1,14 @@
 interface BubbleProps {
   children: React.ReactNode;
+  bgColor?: string;
+  textColor?: string;
 }
 
 const Bubble = (props: BubbleProps) => {
   return (
-    <div className="bg-slate-800 p-4 rounded-lg shadow-lg relative w-max">
-      <p className="text-lg font-medium text-slate-100">{props.children}</p>
-      <div className="absolute bg-slate-800 w-5 h-5 rotate-45 right-4 "></div>
+    <div className={`bg-${props.bgColor || "slate"}-800 p-4 rounded-lg shadow-lg relative w-max`}>
+      <p className={`text-lg font-medium text-${props.textColor || "white"}-100"`}>{props.children}</p>
+      <div className={`absolute bg-${props.bgColor || "slate"}-800 w-5 h-5 rotate-45 right-4 `}></div>
     </div>
   );
 };
--- src/templates/Header.tsx
+++ src/templates/Header.tsx
@@ -1,21 +1,32 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faHome, faUser, faCogs, faEnvelope, faClose } from '@fortawesome/free-solid-svg-icons';
+
 const Header = () => {
-  return (
-    <div className=" py-5 relative rgba(0, 0, 0, 0.5)">
-        <div className="container mx-auto">
-            <div className="flex justify-between items-center">
-            <h1 className="text-3xl font-bold text-slate-300">@mvadly</h1>
-            <nav>
-                <ul className="flex space-x-4">
-                <li><a href="#home" className="text-gray-600 hover:text-white">Home</a></li>
-                <li><a href="#about" className="text-gray-600 hover:text-white">About</a></li>
-                <li><a href="#services" className="text-gray-600 hover:text-white">Services</a></li>
-                <li><a href="#contact" className="text-gray-600 hover:text-white">Contact</a></li>
-                </ul>
-            </nav>
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+    return (
+        <div className="py-2 sm:py-5 relative rgba(0, 0, 0, 0.5)">
+            <div className="container mx-auto">
+                <div className={`flex justify-between items-center`}>
+                    <h1 className="px-3 sm:px-0 text-2xl font-bold text-slate-300">@mvadly</h1>
+                    <div className={`${!isMobileMenuOpen ? "":"absolute right-0"} block sm:hidden px-2 cursor-pointer`} onClick={toggleMobileMenu}>
+                    {!isMobileMenuOpen ? <FontAwesomeIcon icon={faBars} /> : <FontAwesomeIcon icon={faClose} />}
+                    </div>
+                    <nav className={`${!isMobileMenuOpen ? "hidden sm:block" : "block sm:hidden"}`}>
+                        <ul className={`${!isMobileMenuOpen ? "sm:flex sm:space-x-4 " : "sm:hidden absolute right-0 top-12 bg-orange-500 px-2 py-3 z-1 rounded-b-xl"} text-slate-200`}>
+                            <li className="mb-4 mx-2 sm:mb-0 sm:mx-4"><a href="#home"><FontAwesomeIcon icon={faHome} className="sm:!hidden" /><span className="!hidden sm:!block">Home</span></a></li>
+                            <li className="mb-4 mx-2 sm:mb-0 sm:mx-4"><a href="#about"><FontAwesomeIcon icon={faUser} className="sm:!hidden" /><span className="!hidden sm:!block">About</span></a></li>
+                            <li className="mb-4 mx-2 sm:mb-0 sm:mx-4"><a href="#services"><FontAwesomeIcon icon={faCogs} className="sm:!hidden" /><span className="!hidden sm:!block">Services</span></a></li>
+                            <li className="mb-4 mx-2 sm:mb-0 sm:mx-4"><a href="#contact"><FontAwesomeIcon icon={faEnvelope} className="sm:!hidden" /><span className="!hidden sm:!block">Contact</span></a></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
