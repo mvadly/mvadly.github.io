f65a3df052f9ec210f77134bf134afce328608d2
Muhammad Fadly <mfadly24@gmail.com>
2025-05-11 00:32:55 +0700

    fix: update header

--- src/templates/Header.tsx
+++ src/templates/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
     };
     return (
-        <div className="w-full py-2 sm:py-5 fixed top-0 bg-slate-800 z-50">
+        <div className="w-full py-2 sm:py-5 fixed top-0 bg-slate-800 border-b-2 sm:border-0 border-white z-50">
             <div className="container mx-auto">
                 <div className={`flex justify-between items-center`}>
                     <h1 className="px-3 sm:px-0 text-2xl font-bold text-slate-300">@mvadly</h1>
@@ -16,7 +16,7 @@ const Header = () => {
                     {!isMobileMenuOpen ? <FontAwesomeIcon icon={faBars} /> : <FontAwesomeIcon icon={faClose} />}
                     </div>
                     <nav className={`${!isMobileMenuOpen ? "hidden sm:block" : "block sm:hidden"}`}>
-                        <ul className={`${!isMobileMenuOpen ? "sm:flex sm:space-x-4 " : "sm:hidden absolute right-0 top-12 bg-orange-500 px-2 py-3 z-10 rounded-b-xl"} text-slate-200`}>
+                        <ul className={`${!isMobileMenuOpen ? "sm:flex sm:space-x-4 " : "sm:hidden absolute right-0 top-12 bg-orange-500 px-2 py-3 z-10 rounded-b-xl border-2 border-white"} text-slate-200`}>
                             <li className="mb-4 mx-2 sm:mb-0 sm:mx-4"><a href="#home"><FontAwesomeIcon icon={faHome} className="sm:!hidden" /><span className="!hidden sm:!block">Home</span></a></li>
                             <li className="mb-4 mx-2 sm:mb-0 sm:mx-4"><a href="#about"><FontAwesomeIcon icon={faUser} className="sm:!hidden" /><span className="!hidden sm:!block">About</span></a></li>
                             <li className="mb-4 mx-2 sm:mb-0 sm:mx-4"><a href="#experiences"><FontAwesomeIcon icon={faCogs} className="sm:!hidden" /><span className="!hidden sm:!block">Experiences</span></a></li>
